790ad80035d602c3b35087d716812fe95f57c856
Robin Green <greenrd@greenrd.org>
2009-04-13 06:19:05 -0700

    Remove TH imports, and add any qualified imports needed by generated code
    Ignore-this: dccb39b17c16c140282592ef26e10488
    
    darcs-hash:20090413131905-f04ea-177229d0076ab1d174c4cfa0f55c9abc55f11209.gz

--- src/Zeroth.hs
+++ src/Zeroth.hs
@@ -8,7 +8,8 @@ import System.IO             ( hPutStr, hClose, hGetContents, openTempFile, stdi
 import System.Directory      ( removeFile, getTemporaryDirectory )
 import System.Exit           ( ExitCode (..) )
 import Control.Monad         ( when )
-import Data.Maybe            ( mapMaybe )
+import Data.List             ( find, isPrefixOf, nub )
+import Data.Maybe            ( isJust, mapMaybe )
 
 import Comments              ( parseComments, mixComments )
 
@@ -35,14 +36,30 @@ zeroth ghcPath cpphsPath ghcOpts cpphsOpts inputFile
                      ParseOk m@(Module _ _ _ _ _ _ decls) -> runTH ghcPath m ghcOpts (mapMaybe getTH decls)
                      e -> error (show e)
          zerothData <- case parseModule zerothInput of
-                         ParseOk (Module loc m opts mWarn exports im decls) -> return (Module loc m opts mWarn exports im (filter delTH decls))
+                         ParseOk (Module loc m opts mWarn exports im decls)
+                           -> return (Module loc m opts mWarn exports (postProcessImports im $ snd thData) (filter delTH decls))
                          e -> error (show e)
          when (inputFile == "-") $ removeFile inputFile2
-         return (prettyPrint zerothData ++ "\n" ++ unlines (mixComments (parseComments input) thData))
+         return (prettyPrint zerothData ++ "\n" ++ unlines (mixComments (parseComments input) $ fst thData))
     where getTH (SpliceDecl l s) = Just (l,s)
           getTH _ = Nothing
           delTH (SpliceDecl _ _) = False
           delTH _ = True
+
+-- Removes TH imports, and adds any qualified imports needed by generated TH code
+postProcessImports :: [ImportDecl] -> [String] -> [ImportDecl]
+postProcessImports oldImports qNames
+   = nub $  removeTH
+        ++ ( map (\q -> ImportDecl { importLoc = emptySrcLoc
+                                   , importModule = ModuleName q
+                                   , importQualified = True
+                                   , importSrc = False
+                                   , importAs = Nothing
+                                   , importSpecs = Nothing })
+            $ filter (\q -> not $ contains (maybe False (\(ModuleName m) -> m == q) . importAs) removeTH) qNames )
+  where
+    removeTH = filter (not . (\(ModuleName m) -> "Language.Haskell.TH" `isPrefixOf` m) . importModule) oldImports
+    contains = (isJust .) . find
 
 preprocessCpphs :: FilePath -- ^ Path to cpphs
                 -> [String]
@@ -62,7 +79,7 @@ runTH :: FilePath -- ^ Path to GHC
       -> Module 
       -> [String]
       -> [(SrcLoc,Splice)]
-      -> IO [(Int,String)]
+      -> IO ([(Int,String)], [String])
 runTH ghcPath (Module _ _ _ _ _ imports _) ghcOpts th
     = do tmpDir <- getTemporaryDirectory
          (tmpInPath,tmpInHandle) <- openTempFile tmpDir "TH.source.zeroth.hs"
@@ -87,17 +104,22 @@ runTH ghcPath (Module _ _ _ _ _ imports _) ghcOpts th
                                         [(ret,_)] -> return ret
                                         _         -> error $ "Failed to parse result:\n"++output
     where thImport = ImportDecl emptySrcLoc (ModuleName "Language.Haskell.TH") False False Nothing Nothing
-          emptySrcLoc = SrcLoc "" 0 0
           pp :: (Pretty a) => a -> String
           pp = prettyPrintWithMode (defaultMode{layout = PPInLine})
           realM = unlines $ ["module Main ( main ) where"]
                             ++ map pp (thImport:imports)
+                            ++ ["import qualified Data.Generics.Schemes"]
+                            ++ ["import qualified Data.Maybe"]
                             ++ ["main = do decls <- sequence $ " ++ pp (List splices)]
-                            ++ ["          print $ map (\\(l,d) -> (l,pprint d)) (zip "++ pp (List lineNums) ++" decls)"]
+                            ++ ["          print $ ( map (\\(l,d) -> (l,pprint d)) (zip " ++ pp (List lineNums) ++ " decls)"]
+                            ++ ["                  , map (Data.Maybe.fromJust . nameModule) $ Data.Generics.Schemes.listify (Data.Maybe.isJust . nameModule) decls)"]
           splices = flip map th $ \(_src,splice) -> App (Var (UnQual (Ident "runQ"))) (Paren (spliceToExp splice))
           lineNums = flip map th $ \(loc,_splice) -> Lit (Int (fromIntegral (srcLine loc)))
           spliceToExp (ParenSplice e) = e
           spliceToExp _ = error "TH: FIXME!"
+
+emptySrcLoc :: SrcLoc
+emptySrcLoc = SrcLoc "" 0 0
 
 {-
 
