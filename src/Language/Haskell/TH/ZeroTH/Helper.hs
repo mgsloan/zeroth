597532fdb009d66fc1b293dd93960eeb798483df
Adam Vogt <vogt.adam@gmail.com>
2014-01-20 13:17:13 -0500

    replace top-level NameU with NameS
    This lets zeroth process like:
    
    {-# LANGUAGE TemplateHaskell #-}
    module Main where
    
    [d| x = 1 |]
    
    main :: IO()
    main = print x
    
    Previously you'd get a `x_0 = 1', which makes the generated file fail at
    finding `x'

--- src/Language/Haskell/TH/ZeroTH/Helper.hs
+++ src/Language/Haskell/TH/ZeroTH/Helper.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE PatternGuards #-}
 module Language.Haskell.TH.ZeroTH.Helper ( helper, idPrefix ) where
 
-import Control.Applicative        ( (<$>) )
-import Data.Generics.Schemes      ( listify )
+import Data.Generics
 import Data.Maybe                 ( fromJust, isJust )
-import Language.Haskell.TH.Ppr    ( pprint )
-import Language.Haskell.TH.Syntax ( Dec, nameModule, Q, runIO )
 import System.IO                  ( hFlush, stdout )
 
 import Language.Haskell.TH.ZeroTH.Comments ( Location )
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+import qualified Data.Set as S
 
 idPrefix :: String
 idPrefix = "ZEROTH OUTPUT: "
@@ -15,6 +16,44 @@ idPrefix = "ZEROTH OUTPUT: "
 helper :: Q [Dec] -> Location -> Q [Dec]
 helper splice loc = do
     decls <- splice
-    runIO $ do putStrLn $ idPrefix ++ show ((loc, unlines $ pprint <$> decls), map (fromJust . nameModule) $ listify (isJust . nameModule) decls)
+    runIO $ do putStrLn $ idPrefix ++ show ((loc, unlines $ fmap pprint $ map unNameU decls),
+                                map (fromJust . nameModule) $ listify (isJust . nameModule) decls)
                hFlush stdout
     return decls
+
+-- | if a Dec binds a name that is a NameU, ghc generates a NameS
+unNameU :: Dec -> Dec
+unNameU x = everywhere (mkT $ \n -> if S.member n u
+                    then renameU n
+                    else n)
+                x
+    where u = S.fromList (topLevelBindrs x)
+
+topLevelBindrs :: Dec -> [Name]
+topLevelBindrs x = case x of
+    FunD n _            -> [n]
+    ValD p _ _          -> allVarPNameU p 
+    DataD _ n _ _ _     -> [n]
+    NewtypeD _ n _ _ _  -> [n]
+    TySynD n _ _        -> [n]
+    ClassD _ n _ _ _    -> [n] -- needed?
+    SigD n _            -> [n]
+    InfixD _ n          -> [n]
+    FamilyD _ n _ _     -> [n]
+    DataInstD _ n _ _ _ -> [n]
+    NewtypeInstD _ n _ _ _ -> [n]
+    TySynInstD n _ _    -> [n]
+    PragmaD (InlineP n _ _ _)       -> [n]
+    PragmaD (SpecialiseP n _ _ _)   -> [n]
+    InstanceD {}    -> []
+    ForeignD {}     -> []
+    PragmaD {}      -> []
+
+renameU :: Name -> Name
+renameU (Name occ (NameU _)) = Name occ NameS
+renameU x = x
+
+allVarPNameU :: Data a => a -> [Name]
+allVarPNameU = everything (++) (mkQ [] $ \e -> case e of
+    VarP n | Name _ (NameU _) <- n -> [n]
+    _ -> [])
